41f870907aaa7e55e671fefbd51020e6e95f9a1b
Jeremy Wells <jswe227@gmail.com>
2022-10-15 20:27:55 -0400

    add lua language server

--- lua/plugin-setup.lua
+++ lua/plugin-setup.lua
@@ -32,9 +32,7 @@ require('lualine').setup{
         lualine_y = {},
         lualine_z = {}
       },
-      tabline = {
-				lualine_a = {'buffers'},
-        lualine_b = {},
+      tabline = { lualine_a = {'buffers'}, lualine_b = {},
         lualine_c = {},
 
 
@@ -172,7 +170,38 @@ require('lspconfig')['rust_analyzer'].setup{
 					},
 				}
 			}
-} 
+}
+local runtime_path = vim.split(package.path, ";")
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+require('lspconfig').sumneko_lua.setup({
+on_attach = on_attach,
+settings = {
+    Lua = {
+    runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = "LuaJIT",
+        -- Setup your lua path
+        path = runtime_path,
+    },
+    diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { "vim" },
+    },
+    workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+    },
+    -- Do not send telemetry data containing a randomized but unique identifier
+    telemetry = {
+        enable = false,
+    },
+    },
+},
+})
+
+
 --require('lspconfig').vimls.setup{
 --	on_attach=on_attach
 --}
